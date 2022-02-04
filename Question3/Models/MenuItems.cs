9e9d78aecd3aad0b430b1fe4a51583b4f56d740b
Dennis Kamau <34334743+dkamau@users.noreply.github.com>
2022-02-04 23:40:41 +0300

    Create dynamic restaurant menu

---
+++ Question3/Models/MenuItems.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Question3.Models
+{
+    public class Choice
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public bool Checked { get; set; }
+    }
+
+    public class Related
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("choices")]
+        public List<Choice> Choices { get; set; }
+
+        public bool Checked { get; set; }
+    }
+
+    public class MenuItem
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("choices")]
+        public List<Choice> Choices { get; set; }
+
+        [JsonProperty("related")]
+        public List<Related> Related { get; set; }
+
+        public bool Checked { get; set; }
+    }
+}
